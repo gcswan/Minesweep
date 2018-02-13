43d491ae457ad5c9c603b912757707b7ccff8c3b
Greg Swan <greg.c.swan@gmail.com>
2018-02-13 12:48:46 -0600

    configured react app from scratch and ran npm build to no avail, working on it

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => 
+    <div> 
+        <h1>Hello World</h1>
+        <p>This should be showing some</p>
+    </div>;
+
+
+render(<App />, document.getElementById("root"));
+
